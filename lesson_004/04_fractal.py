7ee500278c0c2cefb74fae0e08e9b7df952c2fb5
berti <1@yandex.ru>
2022-08-09 03:20:25 +0300

    Not Done #4.4. To hard understand recursion

--- lesson_004/04_fractal.py
+++ lesson_004/04_fractal.py
@@ -26,8 +26,39 @@ import simple_draw as sd
 
 # можно поиграть -шрифтами- цветами и углами отклонения
 
-# TODO здесь ваш код
+# def draw_branches(point, length, angle=30):
+#     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+#     v2 = sd.get_vector(start_point=point, angle=180-angle, length=length, width=3)
+#     v1.draw()
+#     v2.draw()
+#     return v1.end_point, v2.end_point
 
+# def draw_branches(point, length, angle=30):
+#     if length < 10:
+#         return
+#     else:
+#         v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=1)
+#         v2 = sd.get_vector(start_point=point, angle=180-angle, length=length, width=1)
+#         v1.draw()
+#         v2.draw()
+#         draw_branches(v1.end_point, length * .75, angle)
+#         draw_branches(v2.end_point, length * .75, 180 - angle)
+
+def draw_branches(point, length, angle):
+    if length < 10:
+        return
+    else:
+        v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=1)
+        v2 = sd.get_vector(start_point=point, angle=180-angle, length=length, width=1)
+        v1.draw()
+        v2.draw()
+        length_koef = (sd.random_number(int(75 - 75*.2), int(75 + 75*.2))) / 100  # получает рандомную длину в диапазоне 20 % от коэффициэнта 0,75
+        angle = sd.random_number(int(30 - 30 * .4), int(30 + 30 * .4))
+        draw_branches(v1.end_point, length * length_koef, angle)
+        draw_branches(v2.end_point, length * length_koef, 180 - angle)
+
+point_0 = sd.get_point(300, 30)
+draw_branches(point_0, 150, 30)
 # 4) Усложненное задание (делать по желанию)
 # - сделать рандомное отклонение угла ветвей в пределах 40% от 30-ти градусов
 # - сделать рандомное отклонение длины ветвей в пределах 20% от коэффициента 0.75
